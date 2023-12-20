0b0511b91af8f6e0a7c779c3258a44664085ab97
Le Yu <yul@yulM4VWW.vmware.com>
2023-12-20 14:31:04 +0800

    Initialize test.c

--- test.c
+++ test.c
@@ -1,12 +1,6 @@
-# SQL Injection
-int main(int argc, char** argv) {
-  char *userName = argv[2];
-
-  // GOOD
-  char userNameSql[1000] = {0};
-  encodeSqlString(userNameSql, 1000, userName);
-  char query2[1000] = {0};
-  sprintf(query2, "SELECT UID FROM USERS where name = \"%s\"", userNameSql);
-  runSql(query2);
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!");
+   return 0;
 }
-
